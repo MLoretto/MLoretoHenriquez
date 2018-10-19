7e16b0b826f59816c866efb7fb36727d259c44f4
MLoretto <loreto.henriquez.m@gmail.com>
2018-10-19 09:43:28 -0300

    Arreglo boton enviar

--- src/js/mail.js
+++ src/js/mail.js
@@ -1,19 +1,27 @@
 const myform = $("form#myform");
 myform.submit(function(event){
   event.preventDefault();
-  console.log("Entramos por aqui ");
   let service_id = "gmailloreto";
   let template_id = "template_7fj4mFVW";
 
-  myform.find("button").text("enviado...");
+  myform.find("button").text("Enviar");
   emailjs.sendForm(service_id,template_id,myform[0]) //Promesa que llama a la api que envía el e-mail
-      .then(function(){ //si todo esta ok, envía el mail y pasa por aquí
-    	alert("Su mensaje ha sido enviado con éxito!");
-       myform.find("button").text("Send");
+      .then(function(){ //si todo esta ok, envía el mail
+      myform.find("button").text("Enviar");
+      limpiar();
+      alert("Su mensaje ha sido enviado con éxito!");
+      
+     
     }, function(err) {
-        console.log("Error " + JSON.stringify(err));
-       alert("Error al enviar el mensaje!\r\n Respuesta:\n " + JSON.stringify(err));
-       myform.find("button").text("Enviar");
+      alert("Error al enviar el mensaje!\r\n Respuesta:\n " + JSON.stringify(err));
+      myform.find("button").text("Enviar");
     });
   return false;
 });
+
+function limpiar(){
+  document.getElementById("fName").value = "";
+  document.getElementById("fMail").value = "";
+  document.getElementById("fMessage").value = "";
+
+}
